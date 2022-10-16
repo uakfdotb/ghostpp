5f01e1c08eac938aa93e134310a5c68193b85a09
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2022-10-16 14:11:22 -0300

    Fix typo.

--- ghost/ghost.cpp
+++ ghost/ghost.cpp
@@ -470,7 +470,7 @@ CGHost :: CGHost( CConfig *CFG )
 	m_Version = "17.2";
 	m_HostCounter = 1;
 	m_AutoHostMaximumGames = CFG->GetInt( "autohost_maxgames", 0 );
-	m_MaxSlaves = CFG->GetInt( "max_slaves", 0 );
+	m_MaxSlaves = CFG->GetInt( "bot_max_slaves", 0 );
 	m_SlaveStartingPort = CFG->GetInt( "bot_slave_starting_port", 0 );
 	m_MaxSlots = CFG->GetInt( "max_slots", 24 );
 	if ( !m_MaxSlots )
